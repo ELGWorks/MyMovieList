e415d232134e36798224a337eacc777b5f478248
ELGWorks <244444991+ELGWorks@users.noreply.github.com>
2026-05-23 10:27:03 +0800

    initial commit

---
+++ Frontend/src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "6467b6ca23a0dcc45549ff2b3f82c8a1";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json()
+    return data.results
+};
